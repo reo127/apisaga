12488a0c9baa5be571e88bec41eb67073cec308e
Rohan malo <64rohanmalo@gmail.com>
2024-06-09 22:35:14 +0530

    random product api added

--- controllers/publicAPIsControllers.js
+++ controllers/publicAPIsControllers.js
@@ -1,5 +1,6 @@
 const jokes = require("../json/jokes.json");
 const quotes = require("../json/quotes.json");
+const products = require("../json/products.json");
 
 const jokesController = (req, res) => {
     try {
@@ -19,4 +20,14 @@ const quoteController = (req, res) => {
     }
 }
 
-module.exports = { jokesController, quoteController };
+
+const randomProductController = async(req, res) => {
+    try {
+        const randomIndex = Math.floor(Math.random() * products.length);
+        res.status(200).json(products[randomIndex])
+    } catch (error) {
+        res.status(400).json(error.message || "something wrond");
+    }
+}
+
+module.exports = { jokesController, quoteController, randomProductController };
--- router/publicAPIsRotuer.js
+++ router/publicAPIsRotuer.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { jokesController, quoteController } = require('../controllers/publicAPIsControllers');
+const { jokesController, quoteController, randomProductController } = require('../controllers/publicAPIsControllers');
 const router = express.Router();
 
 router.get("/joke", jokesController);
 router.get("/quote", quoteController);
+router.get("/product", randomProductController);
 
 
 module.exports = router;
