73c6d8aa56d66b2bb6fb644f502c8508201fddea
Rohan malo <64rohanmalo@gmail.com>
2024-06-09 22:27:28 +0530

    quotes api added

--- controllers/publicAPIsControllers.js
+++ controllers/publicAPIsControllers.js
@@ -1,12 +1,22 @@
-const jokes = require("../json/jokes.json")
+const jokes = require("../json/jokes.json");
+const quotes = require("../json/quotes.json");
 
 const jokesController = (req, res) => {
     try {
         const randomIndex = Math.floor(Math.random() * jokes.length);
-        res.status(200).json(jokes[randomIndex] )
+        res.status(200).json(jokes[randomIndex])
     } catch (error) {
-        res.status(400).json(error.message || "something wrond")
+        res.status(400).json(error.message || "something wrond");
     }
 }
 
-module.exports = {jokesController}
+const quoteController = (req, res) => {
+    try {
+        const randomIndex = Math.floor(Math.random() * quotes.length);
+        res.status(200).json(jokes[randomIndex])
+    } catch (error) {
+        res.status(400).json(error.message || "something wrond");
+    }
+}
+
+module.exports = { jokesController, quoteController };
--- router/publicAPIsRotuer.js
+++ router/publicAPIsRotuer.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const { jokesController } = require('../controllers/publicAPIsControllers')
-const router = express.Router()
+const express = require('express');
+const { jokesController, quoteController } = require('../controllers/publicAPIsControllers');
+const router = express.Router();
 
-router.get("/joke", jokesController)
+router.get("/joke", jokesController);
+router.get("/quote", quoteController);
 
 
-module.exports = router
+module.exports = router;
