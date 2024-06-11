999bba097054ec399bece458e83b5b880adc3700
Rohan malo <64rohanmalo@gmail.com>
2024-06-11 17:48:57 +0530

    cors added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const cookieParser = require("cookie-parser");
 const todosRouter = require('./router/todosRouter')
@@ -7,7 +8,7 @@ const publicAPIsRotuer = require("./router/publicAPIsRotuer")
 const database = require('./database')
 require('dotenv').config()
 
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
